3c08e74d6eed75c4dad0bc8e6b88bb2427542bf5
Anton tuzov <antontuzovf@gmail.com>
2024-06-30 11:16:59 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Developer portfolio that's a 2D game
+
+- Inspired by the many creative 3D developer portfolios, I create one that's 2D instead. This is meant as a template that you can use to build your own. It works on both desktop and mobile.
+
+![Demo App](/public/kaboom-portfolio.mov)
+
+
+
+-   🌟 Learn vite.js + kaboom.js
+-   🎃 Build Responsive Design
+-   🚀 Deploy for FREE!
+
+# Run Locally
+
+
+### Install dependencies
+
+```shell
+npm install
+```
+
+### Start the app
+
+```shell
+npm run dev
+```
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    Here is my <a href="https://github.com/antontuzov" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
@@ -16,7 +16,7 @@ export const dialogueData = {
   Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/antontuzov" target="_blank">here</a>.`,
   library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
